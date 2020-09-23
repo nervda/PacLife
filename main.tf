d8b72222635396c672d9b3b51abfb530fa41dc14
William Lee <nervda@gmail.com>
2020-09-23 20:31:40 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 resource "null_resource" "cluster" {
     connection {
     host = "bobspec2j.com"
+    
     }
+
+resource "null_resource" "delay" {
+    provisioner "local-exec" {
+    command = "sleep 300"
+}
+}
 
 }

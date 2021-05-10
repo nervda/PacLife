f89c30458dcfa67a0d3124e06c1822fee7df6a32
William Lee <38011201+nervda@users.noreply.github.com>
2021-05-10 09:30:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "delay" {
     provisioner "local-exec" {
-    command = "sleep 300"
+    command = "sleep 3"
 }
 }
 resource "null_resource" "delay1" {
     provisioner "local-exec" {
-    command = "sleep 300"
+    command = "sleep 3"
 }
 }

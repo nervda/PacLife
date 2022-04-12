a89a7f47ffbec1e37bf670d15d8b33db8fa1e051
William Lee <38011201+nervda@users.noreply.github.com>
2022-04-12 14:56:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,43 @@
 
 terraform {
   required_providers {
+#     ad = {
+#       source  = "hashicorp/ad"
+#       version = "0.4.3"
+#     }
     aws = {
-      source = "hashicorp/aws"
-      version = "4.9.0"
+      source  = "hashicorp/aws"
+      version = "3.47.0"
     }
   }
 }
-
-resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  count         = 1
+  # tags = {
+  #   Name = var.instance_names[count.index]
+  # }
+}
+# variable "instance_names" {
+#   type    = list(any)
+#   default = ["dev-instance", "stage-instance", "prod-instance"]
+# }
 
 resource "null_resource" "delay" {
     provisioner "local-exec" {

9d80bf5956a70c75758c68ada9445f4eb172147c
William Lee <38011201+nervda@users.noreply.github.com>
2022-05-25 12:46:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,17 @@ terraform {
       source = "hashicorp/aws"
       version = "4.15.1"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+  provider "null" {
+  # Configuration options
 }
 
 resource "null_resource" "delay" {

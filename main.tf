6f5c78c7ede16430d11f7cf88a94cf9cdb07345d
William Lee <38011201+nervda@users.noreply.github.com>
2022-05-25 12:44:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 #adding comment for a trigger.
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "null_resource" "delay" {
     provisioner "local-exec" {
     command = "sleep 1"

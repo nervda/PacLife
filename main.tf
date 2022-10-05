a0c92d68f78c4b91b1cc5736c4364bb039774b18
William Lee <38011201+nervda@users.noreply.github.com>
2022-10-05 10:12:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "delay" {
     command = "sleep 1"
 }
 }
-resource "null_resource" "delay3" {
+resource "null_resource" "delay2" {
     provisioner "local-exec" {
     command = "sleep 1"
 }
